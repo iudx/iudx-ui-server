24e4419b893230cdc8b916c7b7eda2c283a3eb51
Rnaaz <rohinanaaz@gmail.com>
2020-04-08 20:26:52 +0530

    Fixed multi tags issue (#2)
    WIP: Adding word cloud feature
    WIP: UI for Data Models

--- ui/assets/js/list_script.js
+++ ui/assets/js/list_script.js
@@ -61,16 +61,21 @@ function _get_item_count(__url){
 */
 
 function get_items(_attr_name,_attr_value){
+	var _temp_a_v;
+
 	if(is_attr_empty(_attr_name,_attr_value)){
 		return;
+	
 	}
+
+	_temp_a_v = replace_whiteSpace(_attr_value);
 
 	if(!first_get_item_call_done){
 		first_get_item_call_done=true;
 		display_search_section();
 	}
 
-	var _temp_a_v = _attr_value
+	// var _temp_a_v = _attr_value
 
 	if(_attr_name=="resourceServerGroup"){
 		_attr_value=cat_conf['resource_server_group_head']+_attr_value
@@ -80,11 +85,11 @@ function get_items(_attr_name,_attr_value){
 	
 	$(".se-pre-con").fadeIn("slow");
 	
-	$.get(cat_conf["cat_base_URL"] + "/search?attribute-name=("+_attr_name+")&attribute-value=("+_attr_value+")", function(data) {
+	$.get(cat_conf["cat_base_URL"] + "/search?attribute-name=("+_attr_name+")&attribute-value=(("+_temp_a_v+"))", function(data) {
             // $("#searched_items").text(data);
 		data=JSON.parse(data)
 		set_data_globally(data);
-		$("#retrieved_items_count").html("About " + get_item_count(data) + " results for " + _temp_a_v + " (Attribute: " + _attr_name + ") | Go to <a href='/c/map'>Map View</a>/<a href='/status'>Status View</a>");
+		$("#retrieved_items_count").html("About " + get_item_count(data) + " results for " + _temp_a_v + " (Attribute: " + _attr_name + ") | Go to <a href='/c/map'>Map View</a>/<a href='/status'>Status View</a>/<a href='/c'>HomePage</a>/<a href='https://www.iudx.org.in/overview-of-iudx/'>Overview Of IUDX</a>");
 		$("#searched_items").html(`<div class="container"></div>`);
 		for (var i = 0; i < data.length; i++) {
 			$("#searched_items").append(json_to_htmlcard(data[i]));
@@ -123,9 +128,10 @@ function set_attr_value(__attr_name,__attr_value) {
         }
 
     if($( "#_value" ).is(':visible')){
-        $( "#_value" ).autocomplete({
+			$( "#_value" ).autocomplete({
             source: __attr_value,
             select: function( event, ui ) {
+				console.log(event,ui)
                 get_items(__attr_name, ui["item"]['label'])
             }
         });
